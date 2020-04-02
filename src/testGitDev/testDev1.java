8b60ca133cc464f29bda403a40cb59e93eb94c06
97joker <joker19975535652@163.com>
2020-04-02 11:22:04 +0800

    第一次更改

--- src/testGitDev/testDev1.java
+++ src/testGitDev/testDev1.java
@@ -3,5 +3,7 @@ package testGitDev;
 public class testDev1 {
 	public static void main(String[] args) {
 		System.out.println("开发人员写的一个类");
+		System.out.println("从云上拉下来后更改第一次");
+		System.out.println("测试remotepush");
 	}
 }
